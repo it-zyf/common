baa25cd90dd6417e6e68bb0f6031e1fd25ab36c7
v-zhangyafeng3 <15735653659@163.com>
2021-07-14 15:06:52 +0800

    工具类的添加

---
+++ src/main/java/com/javaboy/common/util/Trans.java
@@ -0,0 +1,21 @@
+package com.javaboy.common.util;
+
+import java.text.DecimalFormat;
+
+public class Trans {
+    public static String FormetFileSize(long file) {
+        DecimalFormat df = new DecimalFormat("#.00");
+        String fileSizeString = "";
+        if (file < 1024) {
+            fileSizeString = df.format((double) file) + "B";
+        } else if (file < 1048576) {
+            fileSizeString = df.format((double) file / 1024) + "K";
+        } else if (file < 1073741824) {
+            fileSizeString = df.format((double) file / 1048576) + "M";
+        } else {
+            fileSizeString = df.format((double) file / 1073741824) + "G";
+        }
+        return fileSizeString;
+    }
+
+}
