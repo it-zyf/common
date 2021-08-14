12e1557b0e1efa5920228c5058e79c6a717493cf
MSI-NB <15735653659@163.com>
2021-08-14 12:13:51 +0800

    本地拉取代码测试

---
+++ src/main/java/com/javaboy/common/controller/hh/GetHh.java
@@ -0,0 +1,24 @@
+package com.javaboy.common.controller.hh;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yayu
+ * @title: GetHh
+ * @description: TODO springboot 通过value获取到一些路径等配置信息
+ * @date 2020/8/14 12:08
+ */
+@RestController
+@RequestMapping("/get")
+public class GetHh {
+    @Value("${test.hh}")
+    private String hh;
+
+    @RequestMapping("/value")
+    public String getHh() {
+        return hh;
+    }
+
+}
