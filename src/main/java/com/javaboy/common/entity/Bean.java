d394a65bc4fff37ce6e12b0504c3c78f689b0334
v-zhangyafeng3 <15735653659@163.com>
2021-08-27 10:28:31 +0800

    Stream 对集合的处理

---
+++ src/main/java/com/javaboy/common/entity/Bean.java
@@ -0,0 +1,16 @@
+package com.javaboy.common.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author: zyf
+ * @create: 2021-08-27 10:13
+ **/
+@Data
+@AllArgsConstructor
+public class Bean {
+    String code;
+    String name;
+    String sex;
+}
