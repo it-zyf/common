1100cf057ae19537634532012c2b751a3e21dae1
v-zhangyafeng3 <v-zhangyafeng3@360.cn>
2021-12-24 16:05:25 +0800

    xxl-job基本使用...定时任务

---
+++ src/main/java/com/javaboy/common/xxl/task/XxlTask.java
@@ -0,0 +1,28 @@
+package com.javaboy.common.xxl.task;
+
+import com.xxl.job.core.context.XxlJobHelper;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: zyf
+ * @create: 2021-12-24 15:40
+ **/
+@Component
+public class XxlTask {
+    /**
+     * 1、简单任务示例（Bean模式）
+     */
+    @XxlJob("big")
+    public void demoJobHandler() throws Exception {
+        XxlJobHelper.log("XXL-JOB, Hello World.");
+        System.out.println("你是猪");
+    }
+
+    @XxlJob("time")
+    public void LookTime(){
+        XxlJobHelper.log("START TASK!");
+        System.out.println("RUN TIME");
+    }
+
+}
