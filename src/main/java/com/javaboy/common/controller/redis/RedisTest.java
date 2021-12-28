49f7faeb252b72f3d1fad91c7d5c675c47c6b54a
v-zhangyafeng3 <v-zhangyafeng3@360.cn>
2021-12-28 17:16:34 +0800

    hutool测试类

--- src/main/java/com/javaboy/common/controller/redis/RedisTest.java
+++ src/main/java/com/javaboy/common/controller/redis/RedisTest.java
@@ -1,7 +1,7 @@
 package com.javaboy.common.controller.redis;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +14,12 @@ import java.util.concurrent.TimeUnit;
 @RequestMapping("/redis")
 public class RedisTest {
   @Autowired
-  RedisTemplate redisTemplate;
+  StringRedisTemplate redisTemplate;
 
     @RequestMapping("/test")
     public String test() {
         redisTemplate.opsForValue().set("name","张三");
-        String name = (String) redisTemplate.opsForValue().get("name");
+        String name = redisTemplate.opsForValue().get("name");
         return name;
 
     }
