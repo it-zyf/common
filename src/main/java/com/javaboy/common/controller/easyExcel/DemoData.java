b98a5acb69144d81be49cb6ea00455d2dde552fb
v-zhangyafeng3 <v-zhangyafeng3@360.cn>
2022-01-04 11:31:03 +0800

    easyExcel写了一半.....后续编写完成.

---
+++ src/main/java/com/javaboy/common/controller/easyExcel/DemoData.java
@@ -0,0 +1,20 @@
+package com.javaboy.common.controller.easyExcel;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+/**
+ * @author: zyf
+ * @create: 2021-12-31 14:45
+ **/
+@Getter
+@Setter
+@EqualsAndHashCode
+public class DemoData {
+    private String string;
+    private Date date;
+    private Double doubleData;
+}
---
+++ src/main/java/com/javaboy/common/controller/easyExcel/ReadTest.java
@@ -0,0 +1,32 @@
+package com.javaboy.common.controller.easyExcel;
+
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.read.listener.PageReadListener;
+import com.alibaba.fastjson.JSON;
+import com.javaboy.common.util.TestFileUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.File;
+
+/**
+ * @author: zyf
+ * @create: 2021-12-31 14:50
+ **/
+@Controller
+@RequestMapping("easyExcel")
+public class ReadTest {
+    @RequestMapping("/test")
+    public void simpleRead() {
+        // 写法1：JDK8+ ,不用额外写一个DemoDataListener
+        // since: 3.0.0-beta1
+        String fileName = TestFileUtil.getPath() + "demo" + File.separator + "demo.xlsx";
+        // 这里 需要指定读用哪个class去读，然后读取第一个sheet 文件流会自动关闭
+        // 这里每次会读取3000条数据 然后返回过来 直接调用使用数据就行
+        EasyExcel.read(fileName, DemoData.class, new PageReadListener<DemoData>(dataList -> {
+            for (DemoData demoData : dataList) {
+                System.out.println("读取到一条数据{}"+ JSON.toJSONString(demoData));
+            }
+        })).sheet().doRead();
+    }
+}
