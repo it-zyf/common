1478b503f069a951d9afdb9c6a2f941cbb7456e8
v-zhangyafeng3 <v-zhangyafeng3@360.cn>
2022-01-06 18:10:10 +0800

    hutool测试类

--- src/main/java/com/javaboy/common/controller/easyExcel/ReadTest.java
+++ src/main/java/com/javaboy/common/controller/easyExcel/ReadTest.java
@@ -2,19 +2,29 @@ package com.javaboy.common.controller.easyExcel;
 
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.read.listener.PageReadListener;
+import com.alibaba.excel.support.ExcelTypeEnum;
 import com.alibaba.fastjson.JSON;
+import com.javaboy.common.util.LocalJsonUtil;
 import com.javaboy.common.util.TestFileUtil;
+import io.swagger.annotations.ApiOperation;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.List;
 
 /**
  * @author: zyf
  * @create: 2021-12-31 14:50
  **/
 @Controller
-@RequestMapping("easyExcel")
+@RequestMapping("/easyExcel")
 public class ReadTest {
     @RequestMapping("/test")
     public void simpleRead() {
@@ -29,4 +39,32 @@ public class ReadTest {
             }
         })).sheet().doRead();
     }
+
+
+    @SneakyThrows(IOException.class)
+    @ApiOperation(value = "导出会员列表Excel")
+    @RequestMapping(value = "/getList", method = RequestMethod.GET)
+    public void exportMemberList(HttpServletResponse response) {
+        setExcelRespProp(response, "会员列表");
+        List<FunctionMenu> memberList = LocalJsonUtil.getListFromJson("json/members.json", FunctionMenu.class);
+        EasyExcel.write(response.getOutputStream())
+                .head(FunctionMenu.class)
+                .excelType(ExcelTypeEnum.XLSX)
+                .sheet("会员列表")
+                .doWrite(memberList);
+    }
+
+
+    /**
+     * 设置excel下载响应头属性
+     */
+    private void setExcelRespProp(HttpServletResponse response, String rawFileName) throws UnsupportedEncodingException {
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        response.setCharacterEncoding("utf-8");
+        String fileName = URLEncoder.encode(rawFileName, "UTF-8").replaceAll("\\+", "%20");
+        response.setHeader("Content-disposition", "attachment;filename*=utf-8''" + fileName + ".xlsx");
+    }
+
+
+
 }
