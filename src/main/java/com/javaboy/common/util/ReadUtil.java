a0c8e3403bd37b58f81c90f82c9f3fd05ef2e021
v-zhangyafeng3 <v-zhangyafeng3@360.cn>
2022-04-24 11:06:07 +0800

    优化读取数据工具类..代码规范.

--- src/main/java/com/javaboy/common/util/ReadUtil.java
+++ src/main/java/com/javaboy/common/util/ReadUtil.java
@@ -2,7 +2,9 @@ package com.javaboy.common.util;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -10,22 +12,33 @@ import java.util.List;
  * @create: 2021-10-20 15:59
  **/
 public class ReadUtil {
+    private ReadUtil() {
+    }
+
     /**
      * 读取文本为一个集合
+     *
      * @param path
      * @return
      * @throws Exception
      */
-    public static List<String> getFileContext(String path) throws Exception {
+    public static List<String> getFileContext(String path) throws IOException {
         FileReader fileReader = new FileReader(path);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
-        List<String> list = new ArrayList<String>();
-        String str = null;
-        while ((str = bufferedReader.readLine()) != null) {
-            if (str.trim().length() > 0) {
-                list.add(str);
+        try {
+            List<String> list = new ArrayList<>();
+            String str = null;
+            while ((str = bufferedReader.readLine()) != null) {
+                if (str.trim().length() > 0) {
+                    list.add(str);
+                }
             }
+            return list;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            bufferedReader.close();
         }
-        return list;
+        return Collections.emptyList();
     }
 }
