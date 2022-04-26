f7bcb6dc105c33f264a1457e1e037b81c7a4b430
v-zhangyafeng3 <v-zhangyafeng3@360.cn>
2022-04-26 16:58:58 +0800

    canal监听数据库变化,发送kafka,kafka进行数据消费....

--- src/main/java/com/javaboy/common/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/javaboy/common/kafka/consumer/KafkaConsumer.java
@@ -1,9 +1,16 @@
 package com.javaboy.common.kafka.consumer;
 
+import cn.hutool.json.JSONUtil;
+import com.javaboy.common.kafka.entity.CanalMessage;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author: zyf
@@ -11,11 +18,33 @@ import org.springframework.stereotype.Component;
  **/
 @Component
 @Slf4j
+@SuppressWarnings("rawtypes")
 public class KafkaConsumer {
+
+    private static final String DELETE="delete";
+
+    @ApiOperation("kafka消费")
     @KafkaListener(topics = "sendMsg")
-    public void listen(ConsumerRecord<?,?> record) {
+    public void listen(ConsumerRecord<?, ?> record) {
         System.out.println(record.value());
         System.out.println(record.topic());
+    }
+
+    @ApiOperation("kafka 消费canal监听数据")
+    @KafkaListener(groupId = "test",topics = "canal_kafka")
+    public void handleDataChange(String message){
+        //将message转换为CanalMessage
+        CanalMessage canalMessage = JSONUtil.toBean(message, CanalMessage.class);
+        String tableName = canalMessage.getTable();
+        log.info("Canal 监听 {} 发生变化；明细：{}", tableName, message);
+        // 业务逻辑自己完善...............
+        if(DELETE.equalsIgnoreCase(canalMessage.getType())){
+            List data = canalMessage.getData();
+            Optional.ofNullable(data).orElseGet(ArrayList::new).forEach(
+                    System.out::println
+            );
+            System.out.println("有人删除表中数据");
+        }
     }
 
 }
