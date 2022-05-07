08ec31e23a2549e336123c9e619e6b96073832a7
v-zhangyafeng3 <v-zhangyafeng3@360.cn>
2022-05-07 18:06:30 +0800

    test分支提交

---
+++ src/main/java/com/javaboy/common/util/Btest.java
@@ -0,0 +1,11 @@
+package com.javaboy.common.util;
+
+/**
+ * @author : zyf
+ * @date : 2022/5/7 18:05
+ */
+public class Btest {
+    public static void main(String[] args) {
+        System.out.println("我是test分支");
+    }
+}
