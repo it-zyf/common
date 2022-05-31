dde0ab600c67c3fd807795c909f584b5d734abb2
v-zhangyafeng3 <v-zhangyafeng3@360.cn>
2022-05-31 17:27:53 +0800

    redisTemplate测试使用.

--- src/main/java/com/javaboy/common/controller/redis/RedisForHash.java
+++ src/main/java/com/javaboy/common/controller/redis/RedisForHash.java
@@ -1,6 +1,7 @@
 package com.javaboy.common.controller.redis;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +16,14 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/redisHash")
+@SuppressWarnings("unchecked")
 public class RedisForHash {
     @Autowired
-    private StringRedisTemplate redisTemplate;
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private RedisTemplate<String,Object> redisTemplate;
+
 
     @RequestMapping("/set")
     public String set() {
@@ -25,9 +31,10 @@ public class RedisForHash {
         map.put("1", "1");
         map.put("2", "2");
         map.put("3", "3");
-        redisTemplate.opsForHash().putAll("userId",map);
+        stringRedisTemplate.opsForHash().putAll("userId",map);
         return "ok";
     }
+
 
     @RequestMapping("/set2")
     public String set2() {
@@ -35,15 +42,52 @@ public class RedisForHash {
         map.put("4", "4");
         map.put("5", "5");
         map.put("6", "6");
-        redisTemplate.delete("userId");
-        redisTemplate.opsForHash().putAll("userId",map);
+        stringRedisTemplate.delete("userId");
+        stringRedisTemplate.opsForHash().putAll("userId",map);
         return "ok";
     }
 
     @RequestMapping("/get")
     public Map get(){
-        HashMap<String, String> map = (HashMap<String, String>) redisTemplate.opsForHash().entries("hh").get("userId");
+        HashMap<String, String> map = (HashMap<String, String>) stringRedisTemplate.opsForHash().entries("hh").get("userId");
         Collection<String> values = map.values();
         return map;
     }
+
+
+
+    @RequestMapping("/addMap")
+    public String add(){
+        HashMap<String, String> map = new HashMap<>(3);
+        map.put("1", "1");
+        map.put("2", "2");
+        map.put("3", "3");
+        redisTemplate.opsForHash().put("singledb_dev","123",map);
+        return "ok";
+    }
+
+
+    @RequestMapping("/addMap2")
+    public String add2(){
+        HashMap<String, String> map = new HashMap<>(3);
+        map.put("4", "4");
+        map.put("5", "5");
+        map.put("6", "6");
+        redisTemplate.opsForHash().put("singledb_dev","456",map);
+        return "ok";
+    }
+
+    @RequestMapping("/getMap2")
+    public Map<String, String> get2(){
+        Map<String, String> singledbDev = (Map<String, String>) redisTemplate.opsForHash().get("singledb_dev", "123");
+        System.out.println(singledbDev);
+        return singledbDev;
+    }
+
+    @RequestMapping("/delMap")
+    public String  delMap(){
+        redisTemplate.opsForHash().delete("singledb_dev","123");
+        return "ok";
+    }
+
 }
