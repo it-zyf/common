06a98f2ca16cfcedb0bc8419351987bea6cb39ea
v-zhangyafeng3 <v-zhangyafeng3@360.cn>
2022-06-13 16:47:21 +0800

    修改备注规范

--- src/main/java/com/javaboy/common/TestController.java
+++ src/main/java/com/javaboy/common/TestController.java
@@ -12,13 +12,19 @@ import io.github.yedaxia.apidocs.DocsConfig;
 public class TestController {
     public static void main(String[] args) {
         DocsConfig config = new DocsConfig();
-        config.setProjectPath("p:\\learnProject\\common"); // 项目根目录
-        config.setProjectName("common"); // 项目名称
-        config.setApiVersion("V1.0");       // 声明该API的版本
-        config.setDocsPath("G:\\test"); // 生成API 文档所在目录
+        // 项目根目录
+        config.setProjectPath("p:\\learnProject\\common");
+        // 项目名称
+        config.setProjectName("common");
+        // 声明该API的版本
+        config.setApiVersion("V1.0");
+        // 生成API 文档所在目录
+        config.setDocsPath("G:\\test");
         config.addJavaSrcPath("P:\\learnProject\\common\\src\\main\\java");
-        config.setAutoGenerate(Boolean.TRUE);  // 配置自动生成
-        Docs.buildHtmlDocs(config); // 执行生成文档
+        // 配置自动生成
+        config.setAutoGenerate(Boolean.TRUE);
+        // 执行生成文档
+        Docs.buildHtmlDocs(config);
 
     }
 }
