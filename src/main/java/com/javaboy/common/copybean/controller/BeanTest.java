6342dcf0f133f0b1b0fba18c179152b2034d7fb5
v-zhangyafeng3 <v-zhangyafeng3@360.cn>
2022-06-15 17:38:42 +0800

    copyBean

--- src/main/java/com/javaboy/common/copybean/controller/BeanTest.java
+++ src/main/java/com/javaboy/common/copybean/controller/BeanTest.java
@@ -25,12 +25,18 @@ public class BeanTest {
 
     @Test
     public void test2() {
+        List<User> userList = createUser();
+        List<PersonDto> persons = BeanConvertUtils.convertListTo(userList, PersonDto::new);
+        Optional.of(persons).orElseGet(ArrayList::new).forEach(System.out::println);
+    }
+
+
+    private List<User> createUser(){
         List<User> userList = Lists.newArrayList();
         User user = User.builder().userName("张三").age("20").sex(1).hobby(Arrays.asList("1", "2", "3")).gay(true).build();
         User user2 = User.builder().userName("李四").age("21").sex(0).hobby(Arrays.asList("1", "2", "3")).gay(false).build();
         userList.add(user);
         userList.add(user2);
-        List<PersonDto> persons = BeanConvertUtils.convertListTo(userList, PersonDto::new);
-        Optional.of(persons).orElseGet(ArrayList::new).forEach(System.out::println);
+        return userList;
     }
 }
