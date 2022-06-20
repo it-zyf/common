15cad6ddaa000471067ddf84055026b3bc7afef2
v-zhangyafeng3 <v-zhangyafeng3@360.cn>
2022-06-20 16:39:53 +0800

    [忽略]佛祖保佑,再无BUG

--- src/main/java/com/javaboy/common/entity/Infomation.java
+++ src/main/java/com/javaboy/common/entity/Infomation.java
@@ -1,7 +1,6 @@
 package com.javaboy.common.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import io.github.yedaxia.apidocs.Ignore;
 import lombok.Builder;
 import lombok.Data;
 
@@ -18,6 +17,6 @@ public class Infomation {
     private String sex; //性别
     private Integer age; //年龄
     private String photo; //照片
-    @JsonFormat(pattern="yyyy-M M-dd HH:mm:ss",timezone = "GMT+8")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date time; //时间
 }
