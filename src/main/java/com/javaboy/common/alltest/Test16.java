28ecb6745f0893b54ea244a9fa9370fbd847e4a0
v-zhangyafeng3 <v-zhangyafeng3@360.cn>
2022-09-05 16:39:43 +0800

    simHash

--- src/main/java/com/javaboy/common/alltest/Test16.java
+++ src/main/java/com/javaboy/common/alltest/Test16.java
@@ -1,12 +1,16 @@
 package com.javaboy.common.alltest;
 
 import cn.hutool.core.bean.BeanUtil;
+import com.alibaba.fastjson.JSON;
 import com.javaboy.common.entity.Woman;
 import com.sun.org.apache.xpath.internal.operations.String;
 import org.junit.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author: zyf
@@ -81,5 +85,31 @@ public class Test16 {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("b");
 
+    }
+
+    @Test
+    public void test6() throws MalformedURLException {
+        java.lang.String a="[{\"value\":\"http://1232safa\",\"key\":\"1\"},{\"value\":\"http://1232safa323\",\"key\":\"1662028486609\"}]";
+        java.lang.String replace = a.replace("[", "").replace("]", "");
+
+
+        java.lang.String s = replace.split("}")[0];
+
+        java.lang.String s3 = new StringBuilder(s).append("}").toString();
+
+        Map map = JSON.parseObject(s3);
+
+        URL value = new URL((java.lang.String) map.get("value"));
+        java.lang.String host = value.getHost();
+        System.out.println(host);
+//
+//        System.out.println(s);
+//        java.lang.String s1 = s.split(",")[0];
+//        java.lang.String s2 = s1.split(":")[1];
+//        System.out.println(s2);
+
+//        URL url = new URL("");
+//        java.lang.String host = url.getHost();
+//        System.out.println(host);
     }
 }
